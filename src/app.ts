96c6f586469c3084558c595886489d03fe84a532
LeonardoMainardes <leonardomainardes168@gmail.com>
2026-04-13 15:33:47 -0300

    feat: autentication routes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,41 @@
 import swagger from "@fastify/swagger"
 import swaggerUI from "@fastify/swagger-ui"
 import Fastify from "fastify"
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod"
+import { appRoutes } from "./routes"
 
 export const app = Fastify({
   logger: true,
+}).withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.setErrorHandler((error, request, reply) => {
+  app.log.error(error)
+  const statusCode = error.statusCode ?? 500
+  reply.status(statusCode).send({
+    message: error.message || "Internal Server Error",
+  })
 })
+
+app.decorateRequest("user")
 
 await app.register(swagger, {
   openapi: {
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
     info: {
       title: "SyncFlow API",
       description: "Documentação da API",
@@ -18,25 +46,10 @@ await app.register(swagger, {
 
 await app.register(swaggerUI, {
   routePrefix: "/docs",
+  uiConfig: {
+    docExpansion: "list",
+    deepLinking: false,
+  },
 })
 
-app.get(
-  "/health",
-  {
-    schema: {
-      description: "Health check",
-      tags: ["Health"],
-      response: {
-        200: {
-          type: "object",
-          properties: {
-            status: { type: "string" },
-          },
-        },
-      },
-    },
-  },
-  async () => {
-    return { status: "ok" }
-  },
-)
+await app.register(appRoutes)
---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,26 @@
+import type { FastifyReply, FastifyRequest } from "fastify"
+import jwt from "jsonwebtoken"
+import { jwtSecret } from "../lib/jwt"
+import { jwtSchema } from "../modules/auth/auth.schema"
+
+export const authMiddleware = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
+  const token = request.headers["authorization"]?.split(" ")[1]
+
+  if (!token) return reply.status(401).send({ message: "Access Denied" })
+
+  try {
+    const verified = jwt.verify(token, jwtSecret)
+
+    const result = jwtSchema.safeParse(verified)
+
+    if (!result.success)
+      return reply.status(400).send({ message: "Bad Request" })
+
+    request.user = result.data
+  } catch (err) {
+    reply.status(401).send({ message: "Invalid Token" })
+  }
+}
